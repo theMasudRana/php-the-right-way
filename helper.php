7d0295a55f0c5f6ab65a9a585a48c1ca4ed682a0
MasudXpeed <mr.masudrana00@gmail.com>
2021-12-16 03:00:27 +0600

    Some things I learned about PHP

---
+++ helper.php
@@ -0,0 +1,7 @@
+<?php
+function pp(array $value)
+{
+    echo "<pre>";
+    print_r($value);
+    echo "</pre>";
+}
---
+++ index.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Learning PHP</title>
+</head>
+<body>
+<?php
+//Variable scope
+/*$x = 5; // Global variable
+echo $x;
+echo '<br>';
+function foo()
+{
+    // we can make x global by using global keyword
+    $x = 12; // Local variable to this function
+    echo $x;
+}
+
+foo();*/
+
+// Anonymous function
+/*$your_age = function ($age) {
+    return $age;
+}; // Must have ; in the end of the brackets.
+echo $your_age(26); // call variable name with function ();*/
+// Callable
+/*$arr = [1, 2, 3, 4];
+$arr2 = array_map(function ($element) {
+    return $element * 2;
+}, $arr);
+
+print_r($arr);
+print_r($arr2);*/
+
+//Arrow function
+/*$array = [1, 3, 4, 5];
+$array2 = array_map(fn($number) => $number * 2, $array);
+print_r($array2)*/
+
+require 'helper.php';
+
+$items = [1, 2, 4, 34, 34, 51];
+$items2 = [4, 5, 6, 6, 7, 8];
+
+pp(array_chunk($items, 2, true));
+//pp(array_combine($items, $items2));
+
+$even = array_filter($items, fn($number) => $number % 2 === 0);
+pp($even);
+
+
+?>
+</body>
+</html>
